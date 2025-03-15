c00e199bff02e92bc5fa53f19a81cdd7e99315e6
massimopallera <pallera.massimo@gmail.com>
2025-03-15 16:36:13 +0100

    initial commit

---
+++ src/main/java/org/lessons/java/spring_pizzeria_crud/SpringPizzeriaCrudApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_pizzeria_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPizzeriaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPizzeriaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/spring_pizzeria_crud/SpringPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_pizzeria_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
