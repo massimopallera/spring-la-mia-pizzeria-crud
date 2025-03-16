3a174eed0fcb4d5e3be625b0b6d12ea9802ed326
massimopallera <pallera.massimo@gmail.com>
2025-03-16 11:37:48 +0100

    add single element page

--- src/main/java/org/lessons/java/spring_pizzeria_crud/Controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_pizzeria_crud/Controller/PizzaController.java
@@ -1,6 +1,7 @@
 package org.lessons.java.spring_pizzeria_crud.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.lessons.java.spring_pizzeria_crud.Model.Pizza;
 import org.lessons.java.spring_pizzeria_crud.Repository.PizzaRepository;
@@ -9,23 +10,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.PathVariable;
 @Controller
-@RequestMapping("/")
+@RequestMapping("/pizze")
 public class PizzaController {
     
     @Autowired
     private PizzaRepository repo;
-    
+
     @GetMapping
     public String index(Model model) {
-
 
         List<Pizza> pizze = repo.findAll();
         model.addAttribute("pizze", pizze);
 
-        return "index";
+        return "pizze/index";
     }
+
+    @GetMapping("/{id}")
+    public String show(Model model, @PathVariable("id") Integer id ) {
+
+        Pizza pizza = repo.findById(id).get();
+
+        model.addAttribute("pizza", pizza);
+
+        return "pizze/show";
+    }
+    
     
 }
